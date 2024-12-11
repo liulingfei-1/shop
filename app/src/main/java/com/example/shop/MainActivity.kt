7cff1330eb07d1f771a4e09bfac934e966cef794
liulingfei-1 <b3679536@qq.com>
2024-12-12 03:27:28 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -13,22 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.shop.ui.theme.MyShopAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyShopAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
